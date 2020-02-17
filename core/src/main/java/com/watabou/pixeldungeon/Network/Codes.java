e0c17d4a87866ed9c6a3be90a47425033529aae8
Nikita22007 <game.nikita22007@yandex.ru>
2020-02-17 21:30:21 +0300

    Network block update

--- core/src/main/java/com/watabou/pixeldungeon/Network/Codes.java
+++ core/src/main/java/com/watabou/pixeldungeon/Network/Codes.java
@@ -7,7 +7,12 @@ package com.watabou.pixeldungeon.Network;
 Прогон через  switch  case
 */
 class Codes {
+    //Network block 0x00-0x0F
     public static final int NOP = 0X00;
+    public static final int SERVER_FULL = 0X01;
+    public static final int SERVER_CLOSED = 0X02;
+
+    //level block 0X10-0X1F
     public static final int LEVEL_MAP = 0X10;
     public static final int LEVEL_VISITED = 0X11;
     public static final int LEVEL_MAPPED = 0X12;
