1c17edb1b710a70e4c50dde3b506499de6e8b51f
Nikita22007 <game.nikita22007@yandex.ru>
2020-05-08 15:13:35 +0300

    Interlevel_Scene_Update

--- core/src/main/java/com/watabou/pixeldungeon/Network/Codes.java
+++ core/src/main/java/com/watabou/pixeldungeon/Network/Codes.java
@@ -20,6 +20,14 @@ class Codes {
     //UI block 0X20-0X3F
     public static final int BOSS_SLAIN    = 0X20;   //server->client
 
+    public static final int IL_DESCEND    = 0x21;   //server->client //IL=INTERLEVEL
+    public static final int IL_FALL       = 0x22;   //server->client
+    public static final int IL_ASCEND     = 0x23;   //server->client
+    public static final int IL_RETURN     = 0x24;   //server->client
+    public static final int IL_RESURRECT  = 0x25;   //server->client
+    public static final int IL_OTHER      = 0x26;   //server->client //text:string
+    public static final int IL_FADE_OUT   = 0x26;   //server->client //when IL loading finished
+
     //Hero block 0x40-0X5F
     public static final int HERO_CLASS    = 0x40;   //0 = random;  1 = warrior; 2=mage; 3=rouge; 4=huntress; other=error+random //client->server
     public static final int HERO_SUBCLASS = 0x41;   //???
@@ -54,6 +62,9 @@ class Codes {
     public static final int BADGE_VICTORY                 = 0x80;
     public static final int BADGE_MASTERY            = 0x81;
 
+    public static final int BADGE_MASTERY            = 0x81;
+
     //unclassed
+
     public static final int ITEM_BROKEN   =0xE0;   //server->client;
 }
