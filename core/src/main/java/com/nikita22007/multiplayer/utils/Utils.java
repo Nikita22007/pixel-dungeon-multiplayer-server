dcbb95caabb7bc2666100a4097d9eb5da51f3797
Nikita22007 <game.nikita22007@yandex.ru>
2023-01-08 19:56:19 +0300

    fix JSON arrays

--- core/src/main/java/com/nikita22007/multiplayer/utils/Utils.java
+++ core/src/main/java/com/nikita22007/multiplayer/utils/Utils.java
@@ -7,9 +7,6 @@ import org.json.JSONException;
 
 public class Utils {
     public static JSONArray putToJSONArray(Object[] array) throws JSONException {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            return new JSONArray(array);
-        }
         JSONArray jsonArray = new JSONArray();
         for (int i = 0; i < array.length; i++) {
             jsonArray.put(i, array[i]);
@@ -17,9 +14,6 @@ public class Utils {
         return jsonArray;
     }
     public static JSONArray putToJSONArray(int[] array) throws JSONException {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            return new JSONArray(array);
-        }
         JSONArray jsonArray = new JSONArray();
         for (int i = 0; i < array.length; i++) {
             jsonArray.put(i, array[i]);
